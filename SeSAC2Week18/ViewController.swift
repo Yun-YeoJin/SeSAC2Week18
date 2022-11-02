071540580988e992bd4d790c409683f8b49f493c
Yun <duwls0349@naver.com>
2022-11-02 10:18:16 +0900

    Initial Commit

---
+++ SeSAC2Week18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2Week18
+//
+//  Created by 윤여진 on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
